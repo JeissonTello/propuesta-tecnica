fc67c39c958da26fa0d06f4c39142c28821d17ff
JeissonTello <111583397+JeissonTello@users.noreply.github.com>
2025-04-11 15:45:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,23 @@ gsap.from(".animated-description", {
   delay: 0.5,
 });
 
+    // Detecta cuando el usuario ha hecho scroll y agrega la clase para animación
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll(".section");
+
+  function checkVisibility() {
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        section.classList.add("animate-on-scroll", "visible");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkVisibility);
+  checkVisibility();  // Para las secciones que ya están visibles al cargar la página
+});
+
 
     botonArriba.addEventListener("click", function () {
         window.scrollTo({ top: 0, behavior: "smooth" });
